dc7a766c40f8500502bebf0ea69f849d66308301
SuprHUlk <ayush123kumar.ak@gmail.com>
2025-07-07 18:21:11 +0530

    fix: Update baseApiUrl and socketBaseUrl to point to the correct chat service

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
     messagingSenderId: '216549739585',
     appId: '1:216549739585:web:e9c28646e474637e3d4ac0',
   },
-  baseApiUrl: 'https://backend-900357939211.asia-south1.run.app',
+  baseApiUrl: 'https://chat-flora-467840372881.asia-south1.run.app',
   showLogs: false,
-  socketBaseUrl: 'wss://backend-900357939211.asia-south1.run.app',
+  socketBaseUrl: 'wss://chat-flora-467840372881.asia-south1.run.app',
 };
